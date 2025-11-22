2be17316c7468772a081402367a6c673474e1d7e
kaneticmomentum-dev <kaneticmomentum-dev@outlook.com>
2025-11-22 22:41:31 +0530

    Inital copy of .NET8 copy

---
+++ WPF Extended DataGrid .NET 8/AdvancedDataGrid/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,36 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Diagnostics;
+
+//using AttachedCommands.ACB;
+
+namespace MultiEventCommand.ViewModels
+{
+    public class DashboardViewModel : ViewModelBase
+    {
+        #region Members
+
+        private RelayCommand _gitHubCommand;
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand GitHubCommandCommand
+        {
+            get { return _gitHubCommand ?? (_gitHubCommand = new RelayCommand(GitHubCommandHandler)); }
+            set { _gitHubCommand = value; }
+        }
+
+        #endregion
+
+        #region Handlers
+
+        private void GitHubCommandHandler()
+        {
+            Process.Start("https://github.com/bhushanpoojary/WpfExtendedDataGrid");
+        }
+
+        #endregion
+    }
+}
---
+++ WPF Extended DataGrid .NET 8/AdvancedDataGrid/Views/Dashboard.xaml.cs
@@ -0,0 +1,25 @@
+﻿using MultiEventCommand.ViewModels;
+using System.Windows;
+
+namespace MultiEventCommand.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashboard
+    {
+        readonly DashboardViewModel _model=new DashboardViewModel();
+        public Dashboard()
+        {
+         
+            DataContext = _model;
+            InitializeComponent();
+            
+        }
+
+        private void RibbonWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Title = "WPF Extended DataGrid " + System.Windows.Forms.Application.ProductVersion;
+        }
+    }
+}
